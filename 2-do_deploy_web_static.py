e9c74a89c8a83698ea243cb90964673ceb8fa139
Atyq96 <Atyqamine1996@gmail.com>
2024-06-02 23:29:46 +0100

    fixing the bug

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.208.83.12', '100.25.158.209']
+env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
